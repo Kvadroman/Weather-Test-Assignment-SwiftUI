db251e089d0e988551eb924a3c1ef7e0143b75dd
Kvadroman <kvadrostoplerman@gmail.com>
2023-06-16 11:15:02 +0300

    Initial Commit

---
+++ Weather-Test-Assignment-SwiftUI/Weather_Test_Assignment_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Test_Assignment_SwiftUIApp.swift
+//  Weather-Test-Assignment-SwiftUI
+//
+//  Created by Ивченко Антон on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Test_Assignment_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
